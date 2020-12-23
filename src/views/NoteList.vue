887e2aa1bbf39a88cb7ae9f0fe932ec47967a757
Eduard <jimlab@mail.ru>
2020-12-23 14:58:23 +0300

    fix color for button

--- src/views/NoteList.vue
+++ src/views/NoteList.vue
@@ -4,7 +4,7 @@
       <div class="text-right absolute">
         <v-dialog v-model="dialog" width="500">
           <template v-slot:activator="{ on, attrs }">
-            <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
+            <v-btn color="accent" dark v-bind="attrs" v-on="on">
               {{ $t("noteList.columnSelect") }}
             </v-btn>
           </template>
@@ -16,64 +16,64 @@
 
             <v-container fill-height>
               <v-checkbox
-                      v-model="selected"
-                      :label="$t('columnHeaders.type')"
-                      value="type"
-                      class="mr-4"
+                v-model="selected"
+                :label="$t('columnHeaders.type')"
+                value="type"
+                class="mr-4"
               ></v-checkbox>
               <v-checkbox
-                      v-model="selected"
-                      :label="$t('columnHeaders.deleted')"
-                      value="deleted"
-                      class="mr-4"
+                v-model="selected"
+                :label="$t('columnHeaders.deleted')"
+                value="deleted"
+                class="mr-4"
               ></v-checkbox>
               <v-checkbox
-                      v-model="selected"
-                      :label="$t('columnHeaders._id')"
-                      value="_id"
-                      class="mr-4"
+                v-model="selected"
+                :label="$t('columnHeaders._id')"
+                value="_id"
+                class="mr-4"
               ></v-checkbox>
               <v-checkbox
-                      :label="$t('columnHeaders.source')"
-                      value="source"
-                      v-model="selected"
-                      class="mr-4"
+                :label="$t('columnHeaders.source')"
+                value="source"
+                v-model="selected"
+                class="mr-4"
               ></v-checkbox>
               <v-checkbox
-                      :label="$t('columnHeaders.user')"
-                      value="user"
-                      v-model="selected"
-                      class="mr-4"
+                :label="$t('columnHeaders.user')"
+                value="user"
+                v-model="selected"
+                class="mr-4"
               ></v-checkbox>
               <v-checkbox
-                      :label="$t('columnHeaders.text')"
-                      value="text"
-                      v-model="selected"
-                      class="mr-4"
+                :label="$t('columnHeaders.text')"
+                value="text"
+                v-model="selected"
+                class="mr-4"
               ></v-checkbox>
               <v-checkbox
-                      :label="$t('columnHeaders.createdAt')"
-                      value="createdAt"
-                      v-model="selected"
-                      class="mr-4"
+                :label="$t('columnHeaders.createdAt')"
+                value="createdAt"
+                v-model="selected"
+                class="mr-4"
               ></v-checkbox>
               <v-checkbox
-                      :label="$t('columnHeaders.updatedAt')"
-                      value="updatedAt"
-                      v-model="selected"
-                      class="mr-4"
+                :label="$t('columnHeaders.updatedAt')"
+                value="updatedAt"
+                v-model="selected"
+                class="mr-4"
               ></v-checkbox>
               <v-checkbox
-                      :label="$t('columnHeaders.user')"
-                      value="used"
-                      v-model="selected"
-                      class="mr-4"
+                :label="$t('columnHeaders.user')"
+                value="used"
+                v-model="selected"
+                class="mr-4"
               ></v-checkbox>
               <v-checkbox
-                      :label="$t('columnHeaders.detail')"
-                      value="detail"
-                      v-model="selected"
-                      class="mr-4"
+                :label="$t('columnHeaders.detail')"
+                value="detail"
+                v-model="selected"
+                class="mr-4"
               ></v-checkbox>
             </v-container>
             <v-divider></v-divider>
@@ -89,11 +89,11 @@
       </div>
     </template>
     <v-data-table
-            :headers="headersLocalize"
-            :items="getNotes"
-            :items-per-page="5"
-            class="elevation-1"
-            :locale="$i18n.locale"
+      :headers="headersLocalize"
+      :items="getNotes"
+      :items-per-page="5"
+      class="elevation-1"
+      :locale="$i18n.locale"
     >
       <template v-slot:item.createdAt="{ item }">
         <span>
@@ -124,60 +124,60 @@
 </template>
 
 <script lang="ts">
-  import { getModule } from "vuex-module-decorators";
-  import NotesModule from "@/store/NotesModule";
-  import { Component, Vue } from "vue-property-decorator";
-  import { Notes } from "@/models/Interfaces";
-  const notesModule = getModule(NotesModule);
+import { getModule } from "vuex-module-decorators";
+import NotesModule from "@/store/NotesModule";
+import { Component, Vue } from "vue-property-decorator";
+import { Notes } from "@/models/Interfaces.ts";
+const notesModule = getModule(NotesModule);
 
-  @Component
-  export default class extends Vue {
-    private selected: Array<string> = ["type", "createdAt", "detail"];
-    private dialog = false;
+@Component
+export default class extends Vue {
+  private selected: Array<string> = ["type", "createdAt", "detail"];
+  private dialog = false;
 
-    get getNotes(): Notes[] {
-      return notesModule.getNotes;
-    }
-    get headersLocalize(): Array<object> {
-      const headers = [
-        { text: this.$t("columnHeaders.type"), value: "type" },
-        { text: this.$t("columnHeaders.deleted"), value: "deleted" },
-        { text: this.$t("columnHeaders._id"), value: "_id" },
-        { text: this.$t("columnHeaders.source"), value: "source" },
-        { text: this.$t("columnHeaders.user"), value: "user" },
-        { text: this.$t("columnHeaders.text"), value: "text" },
-        { text: this.$t("columnHeaders.createdAt"), value: "createdAt" },
-        { text: this.$t("columnHeaders.updatedAt"), value: "updatedAt" },
-        { text: this.$t("columnHeaders.used"), value: "used" },
-        {
-          text: this.$t("columnHeaders.detail"),
-          value: "detail",
-          sortable: false
-        }
-      ];
-      const sortHeaders = () => {
-        return headers.filter(h => this.selected.includes(h.value));
-      };
-      return [
-        {
-          text: this.$t("columnHeaders.textStart"),
-          align: "start",
-          sortable: false,
-          value: "name"
-        },
-        ...sortHeaders()
-      ];
-    }
-    async mounted() {
-      await notesModule.fetchNotes();
-    }
+  get getNotes(): Notes[] {
+    return notesModule.getNotes;
   }
+  get headersLocalize(): Array<object> {
+    const headers = [
+      { text: this.$t("columnHeaders.type"), value: "type" },
+      { text: this.$t("columnHeaders.deleted"), value: "deleted" },
+      { text: this.$t("columnHeaders._id"), value: "_id" },
+      { text: this.$t("columnHeaders.source"), value: "source" },
+      { text: this.$t("columnHeaders.user"), value: "user" },
+      { text: this.$t("columnHeaders.text"), value: "text" },
+      { text: this.$t("columnHeaders.createdAt"), value: "createdAt" },
+      { text: this.$t("columnHeaders.updatedAt"), value: "updatedAt" },
+      { text: this.$t("columnHeaders.used"), value: "used" },
+      {
+        text: this.$t("columnHeaders.detail"),
+        value: "detail",
+        sortable: false
+      }
+    ];
+    const sortHeaders = () => {
+      return headers.filter(h => this.selected.includes(h.value));
+    };
+    return [
+      {
+        text: this.$t("columnHeaders.textStart"),
+        align: "start",
+        sortable: false,
+        value: "name"
+      },
+      ...sortHeaders()
+    ];
+  }
+  async mounted() {
+    await notesModule.fetchNotes();
+  }
+}
 </script>
 
 <style scoped>
-  .absolute {
-    position: fixed;
-    bottom: 1rem;
-    left: 1rem;
-  }
+.absolute {
+  position: fixed;
+  bottom: 1rem;
+  left: 1rem;
+}
 </style>
