19c1e82421f4915934d46ba24b6874ef52ca9064
Eduard <jimlab@mail.ru>
2020-12-23 14:43:30 +0300

    add interfaces

---
+++ src/models/Interfaces.ts
@@ -0,0 +1,14 @@
+export interface Interfaces {}
+export interface Notes {
+  createdAt: string;
+  deleted: boolean;
+  source: string;
+  status: Object;
+  text: string;
+  type: string;
+  updatedAt: string;
+  used: boolean;
+  user: string;
+  __v: number;
+  _id: string;
+}
