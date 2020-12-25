3a059a2b24155b9db38e4340647fda8f3e229acd
Eduard <jimlab@mail.ru>
2020-12-25 15:32:14 +0300

    fix Notes interface

--- src/models/Interfaces.ts
+++ src/models/Interfaces.ts
@@ -1,16 +1,16 @@
-import VueI18n from 'vue-i18n'
+import VueI18n from "vue-i18n";
 
 export interface Headers {
-  text: VueI18n.TranslateResult,
-  align?: string,
-  value: string,
-  sortable?: boolean
+  text: VueI18n.TranslateResult;
+  align?: string;
+  value: string;
+  sortable?: boolean;
 }
 export interface Notes {
   createdAt: string;
   deleted: boolean;
   source: string;
-  status: Object;
+  status: { verified?: boolean; sentCount: number };
   text: string;
   type: string;
   updatedAt: string;
