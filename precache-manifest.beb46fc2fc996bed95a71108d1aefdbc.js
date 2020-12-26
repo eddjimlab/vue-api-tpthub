6c7ceacf8a63b0eea0c00a1ed24b26c35de1ae35
Eduard <jimlab@mail.ru>
2020-12-26 14:50:14 +0300

    commit gh-pages

---
+++ precache-manifest.beb46fc2fc996bed95a71108d1aefdbc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f831fafb120cda9c0340",
+    "url": "/vue-api-tpthub/css/app.9a3fc35e.css"
+  },
+  {
+    "revision": "4d8ffa6070f919cdb989",
+    "url": "/vue-api-tpthub/css/chunk-158300f3.2c168058.css"
+  },
+  {
+    "revision": "69b49dacf1da63b31c86",
+    "url": "/vue-api-tpthub/css/chunk-vendors.1151dbc4.css"
+  },
+  {
+    "revision": "18e064ac6adfc75d0fb67be8e1785e29",
+    "url": "/vue-api-tpthub/index.html"
+  },
+  {
+    "revision": "f831fafb120cda9c0340",
+    "url": "/vue-api-tpthub/js/app.99885013.js"
+  },
+  {
+    "revision": "4d8ffa6070f919cdb989",
+    "url": "/vue-api-tpthub/js/chunk-158300f3.63853705.js"
+  },
+  {
+    "revision": "f5256c59f453c14af0fb",
+    "url": "/vue-api-tpthub/js/chunk-2d0d0007.6f75ed52.js"
+  },
+  {
+    "revision": "c3f441d15910f0cc7d98",
+    "url": "/vue-api-tpthub/js/chunk-781f3572.6bd7c2a9.js"
+  },
+  {
+    "revision": "69b49dacf1da63b31c86",
+    "url": "/vue-api-tpthub/js/chunk-vendors.7e3589ae.js"
+  },
+  {
+    "revision": "ab552b87eadbc9bd9276b3b64bf55a1a",
+    "url": "/vue-api-tpthub/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-api-tpthub/robots.txt"
+  }
+]);
